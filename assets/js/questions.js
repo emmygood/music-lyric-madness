08776677421d083f19b6de71e86a135600851753
Emmy Good <emilyg405@gmail.com>
2023-05-20 17:29:05 +0000

    Add function to start quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+/* jshint esversion: 11 */
+
+const questions = [
+    {
+        question: "aaa",
+        answers: [
+            {text: "1", correct: true},
+            {text: "2", correct: false},
+            {text: "3", correct: false},
+            {text: "4", correct: false}
+        ]
+    },
+    {
+        question: "bbb",
+        answers: [
+            {text: "5", correct: false},
+            {text: "6", correct: true},
+            {text: "7", correct: false},
+            {text: "8", correct: false}
+        ]
+    }
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,47 @@
+/* jshint esversion: 11 */
+
 let startButton = document.getElementById('start-button');
 let questionArea = document.getElementById('question-cont');
+let questionDiv = document.getElementById('question');
+let answerBtns = document.getElementById('answer-buttons');
+
+let questionsShuffled = null;
+let questionNumber = 0;
 
 startButton.addEventListener('click', startQuiz);
+
+function shuffleQuestions() {
+    questionsShuffled = questions.sort(() => Math.random() - 0.5);
+}
 
 
 function startQuiz() {
     console.log('Game started');
-    startButton.classlist.add('hide');
+    startButton.classList.add('hide');
     questionArea.classList.remove('hide');
+    shuffleQuestions();
+    nextQuestion();
 
 
 }
 
 function nextQuestion() {
+    displayQuestion(questionsShuffled[questionNumber]);
+
 
 }
+
+function displayQuestion(question) {
+    questionDiv.innerText = question.question;
+    question.answers.forEach(answer => {
+        let btn = document.createElement('button');
+        btn.innerText = answer.text;
+        btn.classList.add('btn');
+        answerBtns.appendChild(btn);
+    });
+
+}
+
 
 function selectAnswer() {
 
