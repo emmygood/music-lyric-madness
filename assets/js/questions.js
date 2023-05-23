f75499411e84a4bab1454aeaa4d700e8f862155d
Emmy Good <emilyg405@gmail.com>
2023-05-23 13:59:21 +0000

    Add more questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -11,6 +11,78 @@ const questions = [
         ]
     },
     {
+        question: "What was the best selling album of 2015?",
+        answers: [
+            {text: "Fetty Wap, Fetty Wap", correct: false},
+            {text: "Adele, 25", correct: true},
+            {text: "Taylor Swift, 1989", correct: false},
+            {text: "Justin Bieber, Purpose", correct: false}
+        ]
+    },
+    {
+        question: "Who is the lead singer of Arctic Monkeys?",
+        answers: [
+            {text: "Jamie Cook", correct: false},
+            {text: "Matt Helders", correct: false},
+            {text: "Nick O'Malley", correct: false},
+            {text: "Alex Turner", correct: true}
+        ]
+    },
+    {
+        question: "Which member of the Foo Fighters was previously the drummer for Nirvana?",
+        answers: [
+            {text: "Taylor Hawkins", correct: false},
+            {text: "Dave Grohl", correct: true},
+            {text: "Nate Mendel", correct: false},
+            {text: "Chris Shiflett", correct: false}
+        ]
+    },
+    {
+        question: "Johnny Cash did a cover of this song written by lead singer of Nine Inch Nails, Trent Reznor.",
+        answers: [
+            {text: "Closer", correct: false},
+            {text: "A Warm Place", correct: false},
+            {text: "Hurt", correct: true},
+            {text: "Big Man with a Gun", correct: false}
+        ]
+    },
+    {
+        question: "Which member of the British pop group 'The Spice Girls'; was known as Ginger Spice?",
+        answers: [
+            {text: "Melanie Brown", correct: false},
+            {text: "Geri Halliwell", correct: true},
+            {text: "Emma Bunton", correct: false},
+            {text: "Victoria Beckham", correct: false}
+        ]
+    },
+    {
+        question: "bbb",
+        answers: [
+            {text: "5", correct: false},
+            {text: "6", correct: true},
+            {text: "7", correct: false},
+            {text: "8", correct: false}
+        ]
+    },
+    {
+        question: "bbb",
+        answers: [
+            {text: "5", correct: false},
+            {text: "6", correct: true},
+            {text: "7", correct: false},
+            {text: "8", correct: false}
+        ]
+    },
+    {
+        question: "bbb",
+        answers: [
+            {text: "5", correct: false},
+            {text: "6", correct: true},
+            {text: "7", correct: false},
+            {text: "8", correct: false}
+        ]
+    },
+    {
         question: "bbb",
         answers: [
             {text: "5", correct: false},
