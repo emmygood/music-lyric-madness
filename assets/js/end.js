eeb727590a4c137167e57438d2c3a005c552c187
Emmy Good <emilyg405@gmail.com>
2023-06-08 10:19:34 +0000

    Add smaller images to end js

--- assets/js/end.js
+++ assets/js/end.js
@@ -15,15 +15,15 @@ let endScore = "";
 if (scoreQuiz <= 4) {
     // response and image for a low score 4 or less
     endScore = "Bad luck! Have another go";
-    endImage.src = 'assets/images/broken.jpg';
+    endImage.src = 'assets/images/broken.jpeg';
 } else if (scoreQuiz >= 5 && scoreQuiz <= 7) {
     // response and image for average score between 5 and 7
     endScore = "You know your stuff!";
-    endImage.src = 'assets/images/microp.jpg';
+    endImage.src = 'assets/images/microp.jpeg';
 } else {
     // response and image for a great score
     endScore = "Wow! Look at you go";
-    endImage.src = 'assets/images/winner1.jpg';
+    endImage.src = 'assets/images/winner1.jpeg';
 }
 
 // taking the final score and displaying the message and image depending on it
